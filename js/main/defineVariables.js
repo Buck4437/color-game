a90f1206a0e1a9009cfb4b36ec9c086e997df880
Buck4437 <Buck4437@users.noreply.github.com>
2020-06-25 10:39:54 +0800

    Refactor

---
+++ js/main/defineVariables.js
@@ -0,0 +1,20 @@
+const defaultSave = {
+  red: 0,
+  redAuto: false,
+  green: 0,
+  greenAuto: false,
+  blue: 0,
+  blueAuto: false,
+  unlocks: {
+   redAuto: true,
+   green: true,
+   greenAuto: true,
+   blue: true,
+   blueAuto: true
+  }
+};
+var player = defaultSave;
+
+var autobuyersInterval = {
+  red: null
+}
