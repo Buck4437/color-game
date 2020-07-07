2cc38f6f24645a6e555ccdd9127f6601088c4fa0
Buck4437 <Buck4437@users.noreply.github.com>
2020-07-07 14:00:11 +0800

    refactor

--- js/main/tabs/tab2/tab2.js
+++ js/main/tabs/tab2/tab2.js
@@ -1,11 +1,11 @@
-function buyUpgrades(category, type, level){
+function buyColorUpgrades(category, type, level){
   for (let currency in upgradesCost[category][type][level]){
     player[currency] -= upgradesCost[category][type][level][currency]
   }
   player.upgrades[category][type] = level
 }
 
-function canBuyUpgrades(category, type, level){
+function canbuyColorUpgrades(category, type, level){
   if(upgradesCost[category][type][level] === undefined){
     return false
   }
@@ -42,91 +42,77 @@ new Vue ({
     },
     upgrades: function(){
       let costParse = this.costParse
-      let canBuyStyle = {
-        color: "white",
-        border: "4px solid white",
-        cursor: "pointer"
+      let styles = {
+        max: {
+          color: "#00FF00",
+          border: "4px solid #00FF00",
+          cursor: "default"
+        },
+        canBuy: {
+          color: "white",
+          border: "4px solid white",
+          cursor: "pointer"
+        },
+        cannotBuy: {
+          color: "grey",
+          border: "4px solid #888888",
+          cursor: "default"
+        }
       }
-      let cannotBuyStyle = {
-        color: "grey",
-        border: "4px solid #888888",
-        cursor: "default"
-      }
-      let maxBuyStyle = {
-        color: "#00FF00",
-        border: "4px solid #00FF00",
-        cursor: "default"
-      }
-      return {
-        redAuto: {
-          text: {
-            data:{
-              locked: "Unlock Red Autoclicker<br><br>Cost: " + costStringify(upgradesCost.red.auto[1]),
-              unlocked: "Upgrade Red Autoclicker <br><br>" + player.upgrades.red.auto + " CPS => " + (player.upgrades.red.auto + 1) + " CPS<br><br>Cost: " + costStringify(upgradesCost.red.auto[player.upgrades.red.auto + 1]),
-              maxed: "Upgrade Red Autoclicker<br><br>"+ player.upgrades.red.auto + " CPS (Maxed!)"
+      return[
+        {
+          id: 0,
+          auto: {
+            color: "red",
+            onclick: function(){
+              if(canbuyColorUpgrades("red", "auto", player.upgrades.red.auto + 1)){
+                buyColorUpgrades("red", "auto", player.upgrades.red.auto + 1)
+                updateAutobuyers()
+              }
             },
-            currentText:  upgradesCost.red.auto[player.upgrades.red.auto + 1] === undefined ? "maxed" : player.upgrades.red.auto == 0 ? "locked" : "unlocked",
+            disabled: !canbuyColorUpgrades("red", "auto", player.upgrades.red.auto + 1),
+            styles: styles
           },
-          onclick: function(){
-            if(canBuyUpgrades("red", "auto", player.upgrades.red.auto + 1)){
-              buyUpgrades("red", "auto", player.upgrades.red.auto + 1)
-              updateAutobuyers()
-            }
-          },
-          disabled: !canBuyUpgrades("red", "auto", player.upgrades.red.auto + 1),
-          style: upgradesCost.red.auto[player.upgrades.red.auto + 1] === undefined ? maxBuyStyle
-               : canBuyUpgrades("red", "auto", player.upgrades.red.auto + 1) ? canBuyStyle
-               : cannotBuyStyle
-        },
-        redMulti: {
-          text: "x2 multiplier to Red gain<br><br>Currently: x" + 2**player.upgrades.red.multi + (upgradesCost.red.multi[player.upgrades.red.multi + 1] === undefined ? "" : ("<br><br>Cost: " + costStringify(upgradesCost.red.multi[player.upgrades.red.multi + 1]))),
-          onclick: function(){
-            if(canBuyUpgrades("red", "multi", player.upgrades.red.multi + 1)){
-              buyUpgrades("red", "multi", player.upgrades.red.multi + 1)
-              updateAutobuyers()
-            }
-          },
-          disabled: !canBuyUpgrades("red", "multi", player.upgrades.red.multi + 1),
-          style: upgradesCost.red.multi[player.upgrades.red.multi + 1] === undefined ? maxBuyStyle
-               : canBuyUpgrades("red", "multi", player.upgrades.red.multi + 1) ? canBuyStyle
-               : cannotBuyStyle
-        },
-        greenAuto: {
-          text: {
-            data:{
-              locked: "Unlock Green Autoclicker<br><br>Cost: " + costStringify(upgradesCost.green.auto[1]),
-              unlocked: "Upgrade Green Autoclicker <br><br>" + player.upgrades.green.auto + " CPS => " + (player.upgrades.green.auto + 1) + " CPS<br><br>Cost: " + costStringify(upgradesCost.green.auto[player.upgrades.green.auto + 1]),
-              maxed: "Upgrade Green Autoclicker<br><br>"+ player.upgrades.green.auto + " CPS (Maxed!)"
+          multi: {
+            color: "red",
+            onclick: function(){
+              if(canbuyColorUpgrades("red", "multi", player.upgrades.red.multi + 1)){
+                buyColorUpgrades("red", "multi", player.upgrades.red.multi + 1)
+                updateAutobuyers()
+              }
             },
-            currentText:  upgradesCost.green.auto[player.upgrades.green.auto + 1] === undefined ? "maxed" : player.upgrades.green.auto == 0 ? "locked" : "unlocked",
-          },
-          onclick: function(){
-            if(canBuyUpgrades("green", "auto", player.upgrades.green.auto + 1)){
-              buyUpgrades("green", "auto", player.upgrades.green.auto + 1)
-              updateAutobuyers()
-            }
-          },
-          isHidden: !player.unlocks.upgrades.green,
-          disabled: !canBuyUpgrades("green", "auto", player.upgrades.green.auto + 1),
-          style: upgradesCost.green.auto[player.upgrades.green.auto + 1] === undefined ? maxBuyStyle
-               : canBuyUpgrades("green", "auto", player.upgrades.green.auto + 1) ? canBuyStyle
-               : cannotBuyStyle
+            disabled: !canbuyColorUpgrades("red", "multi", player.upgrades.red.multi + 1),
+            styles: styles
+          }
         },
-        greenMulti: {
-          text: "x2 multiplier to Green gain<br><br>Currently: x" + 2**player.upgrades.green.multi + (upgradesCost.green.multi[player.upgrades.green.multi + 1] === undefined ? "" : ("<br><br>Cost: " + costStringify(upgradesCost.green.multi[player.upgrades.green.multi + 1]))),
-          onclick: function(){
-            if(canBuyUpgrades("green", "multi", player.upgrades.green.multi + 1)){
-              buyUpgrades("green", "multi", player.upgrades.green.multi + 1)
-              updateAutobuyers()
-            }
+        {
+          id: 1,
+          auto: {
+            color: "green",
+            onclick: function(){
+              if(canbuyColorUpgrades("green", "auto", player.upgrades.green.auto + 1)){
+                buyColorUpgrades("green", "auto", player.upgrades.green.auto + 1)
+                updateAutobuyers()
+              }
+            },
+            isHidden: !player.unlocks.upgrades.green,
+            disabled: !canbuyColorUpgrades("green", "auto", player.upgrades.green.auto + 1),
+            styles: styles
           },
-          isHidden: !player.unlocks.upgrades.green,
-          disabled: !canBuyUpgrades("green", "multi", player.upgrades.green.multi + 1),
-          style: upgradesCost.green.multi[player.upgrades.green.multi + 1] === undefined ? maxBuyStyle
-               : canBuyUpgrades("green", "multi", player.upgrades.green.multi + 1) ? canBuyStyle
-               : cannotBuyStyle
-        },
-      }
-    }
+          multi:{
+            color: "green",
+            onclick: function(){
+              if(canbuyColorUpgrades("green", "multi", player.upgrades.red.multi + 1)){
+                buyColorUpgrades("green", "multi", player.upgrades.red.multi + 1)
+                updateAutobuyers()
+              }
+            },
+            isHidden: !player.unlocks.upgrades.green,
+            disabled: !canbuyColorUpgrades("green", "multi", player.upgrades.red.multi + 1),
+            styles: styles
+          }
+        }
+      ]
+    },
   }
 })
